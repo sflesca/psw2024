ece6ca391d8f5485c099df3b345288fde9a948d0
sflesca <sergio.flesca@unical.it>
2024-02-19 16:49:43 +0100

    Initial commit

---
+++ src/main/java/psw/psw2024/Psw2024Application.java
@@ -0,0 +1,13 @@
+package psw.psw2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Psw2024Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Psw2024Application.class, args);
+    }
+
+}
---
+++ src/test/java/psw/psw2024/Psw2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package psw.psw2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Psw2024ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
