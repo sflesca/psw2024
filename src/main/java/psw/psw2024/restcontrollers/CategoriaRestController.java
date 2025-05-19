7f792a4aa4c4aba91aadde6f64eb017ef1956b36
sflesca <sergio.flesca@unical.it>
2025-05-19 09:59:03 +0200

    jx

--- src/main/java/psw/psw2024/restcontrollers/CategoriaRestController.java
+++ src/main/java/psw/psw2024/restcontrollers/CategoriaRestController.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/categoria")
+@CrossOrigin("*")
 public class CategoriaRestController {
 
     private CategoriaRepository categoriaRepository;
